c74a2e94fa511b3ac69038df1f5676cd01c824e1
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-11-24 11:51:43 +0400

    Create token

--- 04_create_token.ts
+++ 04_create_token.ts
@@ -5,14 +5,15 @@ import { createMint } from "@solana/spl-token";
 
 dotenv.config();
 
-const keypair = process.env.KEYPAIR;
+const keypair = process.env.SECRET_KEY;
 if (!keypair) {
-  console.error("Please provide a keypair");
-  process.exit(1);
+  console.log("Please provide a keypair");
+  console.log("❌ Failed!");
+  process.exit();
 }
 
 const connection = new Connection(clusterApiUrl("devnet"));
-const owner = getKeypairFromEnvironment("KEYPAIR");
+const owner = getKeypairFromEnvironment("SECRET_KEY");
 
 const token = await createMint(connection, owner, owner.publicKey, null, 2);
 const link = getExplorerLink("address", token.toString(), "devnet");
